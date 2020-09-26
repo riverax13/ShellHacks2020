ac3274b0fcb57e4a8f41940e35e98edb916753de
Jeannie Estevez <jeanniestvzz@gmail.com>
2020-09-26 17:10:38 -0400

    Added map

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("my-slides");
+    var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
@@ -35,7 +35,7 @@ showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementsByClassName("my-slides");
+  var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
